55a10b9d264cbf4ef84d1eadc3ae455961442ab8
M. Lane Thompson <65419580+wtrboy@users.noreply.github.com>
2020-06-15 13:57:06 -0400

    Add files via upload

---
+++ Class2Assignment2.java
@@ -0,0 +1,12 @@
+public class Class2Assignment2 
+{
+    public static void main(String args[])
+    {
+    String name="M. Lane Thompson";
+    double age=44;
+    double salary=44000;
+    System.out.println("Name....." + name);
+    System.out.println("Age......" + age);
+    System.out.println("Salary........" + salary);
+    }
+}
